65ac97babb611b02cbb9216089a6df80c1d4110d
Pieter Pas <pieter.pas@outlook.com>
2016-11-28 00:45:04 +0100

    Betere downloadversiechecker

--- testWhileCurl.sh
+++ testWhileCurl.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-version=71
-a=0
-normal=0
-while [ "$a" == "0" ] || [ "$normal" == "0" ] 
+version=$((72))
+result=0
+while [ "$result" == "0" ] 
 do
-    aprev=$a
-    curl -s -I -X GET http://download.blender.org/release/Blender2.${version}/blender-2.${version}-linux-glibc211-x86_64.tar.bz2 | head -n 1 | grep "HTTP/1.[01] 2.."
-    normal=$?
-    curl -s -I -X GET http://download.blender.org/release/Blender2.${version}/blender-2.${version}a-linux-glibc211-x86_64.tar.bz2 | head -n 1 | grep "HTTP/1.[01] 2.."
-    a=$?
-    version=$((version+1)) 
+    
+    curl -s -I -X GET http://download.blender.org/release/Blender2.${version} | head -n 1 | grep "HTTP/1.[01] [23].."
+    result=$?
+    echo -e "2.$version\t$result$"
+    version=$((version+1))
 done
-version=$((version-2)) 
-if [ "$aprev" == "0" ]
-then
-    echo "http://download.blender.org/release/Blender2.${version}/blender-2.${version}a-linux-glibc211-x86_64.tar.bz2"
-else
-    echo "http://download.blender.org/release/Blender2.${version}/blender-2.${version}-linux-glibc211-x86_64.tar.bz2"
-fi
+version=$((version-2))
+letters=("" "a" "b" "c")
+echo ${letters[0]}
+result=0
+index=0
+while [ "$result" == "0" ] 
+do
+    curl -s -I -X GET http://download.blender.org/release/Blender2.${version}/blender-2.${version}${letters[index]}-linux-glibc211-x86_64.tar.bz2 | head -n 1 | grep "HTTP/1.[01] 2.."
+    result=$?
+    echo -e "2.$version${letters[index]}\t$result"
+    index=$((index+1))
+done
+index=$((index-2))
+echo http://download.blender.org/release/Blender2.${version}/blender-2.${version}${letters[index]}-linux-glibc211-x86_64.tar.bz2
